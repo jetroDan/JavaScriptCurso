0184e9f9b84c735f716c67570f9e0039cffe0a83
bench <bench@dfms.com>
2019-09-07 14:36:47 -0500

    cambio1

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,11 @@
 
+var miImage = document.querySelector('img');
+
+miImage.onclick = function () {
+    var miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/image_one.jpg') {
+      miImage.setAttribute('src','images/imege_two.jpg');
+    } else {
+      miImage.setAttribute('src', 'images/image_one.jpg');
+    }
+}
